c6475022b2c21d5ac1797a57dd66392f0deefaf0
Niasi Magalhães <not@comic.com>
2022-06-02 19:12:05 -0300

    UFF-008 - Peças inseridas no tabuleiro

--- xadruff-frontend/src/components/Board/Board.tsx
+++ xadruff-frontend/src/components/Board/Board.tsx
@@ -1,24 +1,36 @@
-
 import "./Board.css";
-import Tile from "../Tile/Tile"
+import Tile from "../Tile/Tile";
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-
 interface Piece {
-  image: string
-  x_pos: number
-  y_pos: number
+  image: string;
+  x_pos: number;
+  y_pos: number;
 }
 
-const pieces: Piece[] =[ ]
+const pieces: Piece[] = [];
 
-for (let i = 0; i < 8; i++){
-  pieces.push({image: 'images/pieces/w_pawn.png', x_pos:i, y_pos:1})
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? "b" : "w";
+  const y_pos = p === 0 ? 7 : 0;
+
+  pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 0, y_pos });
+  pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 7, y_pos });
+  pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 1, y_pos });
+  pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 6, y_pos });
+  pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 2, y_pos });
+  pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 5, y_pos });
+  pieces.push({ image: `images/pieces/${type}_queen.png`, x_pos: 3, y_pos });
+  pieces.push({ image: `images/pieces/${type}_king.png`, x_pos: 4, y_pos });
 }
-for (let i = 0; i < 8; i++){
-  pieces.push({image: 'images/pieces/b_pawn.png', x_pos:i, y_pos:6})
+
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: "images/pieces/w_pawn.png", x_pos: i, y_pos: 1 });
+}
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: "images/pieces/b_pawn.png", x_pos: i, y_pos: 6 });
 }
 
 export default function Board() {
@@ -26,16 +38,16 @@ export default function Board() {
 
   for (let j = verticalAxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalAxis.length; i++) {
-        const number = j + i + 2;
-        let image = "";
+      const number = j + i + 2;
+      let image = "";
 
-        pieces.forEach(p => {
-          if(p.x_pos == i && p.y_pos == j){
-            image=p.image
-          }
-        })
+      pieces.forEach((p) => {
+        if (p.x_pos == i && p.y_pos == j) {
+          image = p.image;
+        }
+      });
 
-        board.push(<Tile image={image} color={number}/>)
+      board.push(<Tile image={image} color={number} />);
     }
   }
 
