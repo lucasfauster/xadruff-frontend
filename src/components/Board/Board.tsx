69a8a71b8c8c22338407dd50c5e691f21c5101ee
luamz <luamsilva@id.uff.br>
2022-06-20 08:18:37 -0300

    UFF-009 - Refatorando render de peças

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -11,28 +11,29 @@ interface Piece {
 
 const pieces: Piece[] = [];
 
-for (let p = 0; p < 2; p++) {
-  const type = p === 0 ? "b" : "w";
-  const y_pos = p === 0 ? 7 : 0;
+function render_pieces(pieces: Piece[], starter: boolean){
+  const w_pawns_y = starter ? 1:6;
+  const b_pawns_y = starter ? 6:1;
 
-  pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 0, y_pos });
-  pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 7, y_pos });
-  pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 1, y_pos });
-  pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 6, y_pos });
-  pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 2, y_pos });
-  pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 5, y_pos });
-  pieces.push({ image: `images/pieces/${type}_queen.png`, x_pos: 3, y_pos });
-  pieces.push({ image: `images/pieces/${type}_king.png`, x_pos: 4, y_pos });
+  const w_pieces_y = starter ? 0:7;
+  const b_pieces_y = starter ? 7:0;
+  for (let color = 0; color < 2; color++) {
+    const type = color === 0 ? "b" : "w";
+    const y_pos = color === 0 ? b_pieces_y : w_pieces_y;
+    const pawns_y = color === 0 ? b_pawns_y : w_pawns_y;
+    pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 0, y_pos });
+    pieces.push({ image: `images/pieces/${type}_rook.png`, x_pos: 7, y_pos });
+    pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 1, y_pos });
+    pieces.push({ image: `images/pieces/${type}_knight.png`, x_pos: 6, y_pos });
+    pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 2, y_pos });
+    pieces.push({ image: `images/pieces/${type}_bishop.png`, x_pos: 5, y_pos });
+    pieces.push({ image: `images/pieces/${type}_queen.png`, x_pos: 3, y_pos });
+    pieces.push({ image: `images/pieces/${type}_king.png`, x_pos: 4, y_pos });
+    for (let i = 0; i < 8; i++) {
+      pieces.push({ image: `images/pieces/${type}_pawn.png`, x_pos: i, y_pos: pawns_y });
+    }
+  }
 }
-
-for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "images/pieces/w_pawn.png", x_pos: i, y_pos: 1 });
-}
-
-for (let i = 0; i < 8; i++) {
-  pieces.push({ image: "images/pieces/b_pawn.png", x_pos: i, y_pos: 6 });
-}
-
 function render_horizontal_border(
   horizontalAxis: string[],
   board: JSX.Element[]
@@ -79,21 +80,22 @@ function render_board(starter: boolean) {
     horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
     addition = 2;
   } else {
-    verticalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-    horizontalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"].reverse();
+    verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"].reverse();;
+    horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"].reverse();
     addition = 1;
   }
 
   render_horizontal_border(horizontalAxis, board);
   render_board_vertical_border(horizontalAxis, verticalAxis, board, addition);
   render_horizontal_border(horizontalAxis, board);
+  render_pieces(pieces, starter);
 
   return board;
 }
 
 export default function Board() {
   const boardRef = useRef<HTMLDivElement>(null);
-  let board = render_board(true);
+  let board = render_board(false);
 
   let activePiece: HTMLElement | null = null;
 
