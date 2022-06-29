77e94b48e0def65a88c76b9b218802c6b55ef386
igorcruzf <igfigueiredo@uolinc.com>
2022-06-29 18:34:36 -0300

    UFF-072 - retornando para url do heroku

--- src/services/ChessService.ts
+++ src/services/ChessService.ts
@@ -1,7 +1,7 @@
 import {ChessResponse} from "../components/Board/Board";
 
 export function startNewGame(start_by: string): Promise<ChessResponse> {
-    return fetch(`http://localhost:8080/chess/new-game?start-by=${start_by}&level=INTERMEDIATE`,
+    return fetch(`https://xadruff-backend.herokuapp.com/chess/new-game?start-by=${start_by}&level=INTERMEDIATE`,
         {
             method: 'POST',
             headers: {
@@ -13,7 +13,7 @@ export function startNewGame(start_by: string): Promise<ChessResponse> {
 }
 
 export function makeMovement(board_id: string, move: string): Promise<ChessResponse> {
-    return fetch(`http://localhost:8080/chess/move?board-id=${board_id}`, {
+    return fetch(`https://xadruff-backend.herokuapp.com/chess/move?board-id=${board_id}`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
