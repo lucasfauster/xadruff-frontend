0b8da874c5f61914fa62f28f195b099c3db99082
igorcruzf <igfigueiredo@uolinc.com>
2022-07-03 15:00:13 -0300

    UFF-080 - criado responsividade para telas de computador de diversos tamanhos

--- src/components/Menu/ColorMenu.tsx
+++ src/components/Menu/ColorMenu.tsx
@@ -11,27 +11,29 @@ export function renderColorMenu(
   return (
     <div id='color-menu'>
       <h2 className={'text'}>COM QUAIS PEÇAS VOCÊ QUER JOGAR?</h2>
-      <div className={'color-option'}>
-        <div className={'color-button'} data-testid="test-pretas"
-             onClick={() => {setColor(false); setCurrentMenu(gameOptionMenu);}}>
-          <img  className={'menu-piece'} src='images/pieces/b_knight.png' alt="Cavalo Preto"/>
+        <div className='color-options'>
+            <div className={'color-option'}>
+                <div className={'color-button'} data-testid="test-pretas"
+                     onClick={() => {setColor(false); setCurrentMenu(gameOptionMenu);}}>
+                    <img  className={'menu-piece'} src='images/pieces/b_knight.png' alt="Cavalo Preto"/>
+                </div>
+                <h3 className={'text'}>PRETAS</h3>
+            </div>
+            <div className={'color-option'}>
+                <div className={'color-button'} data-testid="test-brancas"
+                     onClick={() => {setColor(true); setCurrentMenu(gameOptionMenu);}}>
+                    <img  className={'menu-piece'} src='images/pieces/w_rook.png' alt="Torre Branca"/>
+                </div>
+                <h3 className={'text'}>BRANCAS</h3>
+            </div>
+            <div className={'color-option'}>
+                <div className={'color-button'} data-testid="test-sortear"
+                     onClick={() => { setColor(randomColor()); setCurrentMenu(gameOptionMenu) ;}}>
+                    <img  className={'menu-piece'} src='images/dado.png' alt="Dado de Sorteio"/>
+                </div>
+                <h3 className={'text'}>SORTEAR</h3>
+            </div>
         </div>
-        <h3 className={'text'}>PRETAS</h3>
-      </div>
-      <div className={'color-option'}>
-        <div className={'color-button'} data-testid="test-brancas"
-             onClick={() => {setColor(true); setCurrentMenu(gameOptionMenu);}}>
-          <img  className={'menu-piece'} src='images/pieces/w_rook.png' alt="Torre Branca"/>
-        </div>
-        <h3 className={'text'}>BRANCAS</h3>
-      </div>
-      <div className={'color-option'}>
-        <div className={'color-button'} data-testid="test-sortear"
-             onClick={() => { setColor(randomColor()); setCurrentMenu(gameOptionMenu) ;}}>
-          <img  className={'menu-piece'} src='images/dado.png' alt="Dado de Sorteio"/>
-        </div>
-        <h3 className={'text'}>SORTEAR</h3>
-      </div>
       <button className={'return'} onClick={() => {
         setCurrentMenu(currentMenu-1);
       }}>Voltar
